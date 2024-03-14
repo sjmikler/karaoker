a242cb8e1aee0c8f0f2e1858a471dd64205b923d
Szymon Mikler <sjmikler@gmail.com>
2024-03-14 17:08:24 +0100

    Handle some YT errors

--- tools/youtube.py
+++ tools/youtube.py
@@ -10,7 +10,7 @@ JPG_DIRECTORY = os.path.join(get_constant("DATA_DIRECTORY"), "JPG")
 FINAL_DIRECTORY = os.path.join(get_constant("FINAL_DIRECTORY"))
 
 
-def download_mp4(link, new_title, table=dict()):
+def _download_mp4(link, new_title, table=dict(), authenticate=False, only_audio=False):
     max_resolution = get_constant("PREFERRED_RESOLUTION")
     jpg_destination_path = os.path.join(JPG_DIRECTORY, new_title) + ".jpg"
     mp4_destination_path = os.path.join(MP4_DIRECTORY, new_title) + ".mp4"
@@ -19,19 +19,15 @@ def download_mp4(link, new_title, table=dict()):
     if mp4_ok and os.path.exists(jpg_destination_path):
         return
 
-    try:
-        try:
-            yt = YouTube(link)
-            stream = yt.streams.get_by_resolution(max_resolution) or yt.streams.filter().get_highest_resolution()
+    if authenticate:
+        yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+    else:
+        yt = YouTube(link)
 
-        except exceptions.AgeRestrictedError:
-            # if the video is age restricted, we need to authenticate
-            if not get_constant("INTERACTIVE_AUTHENTICATION"):
-                return 2
-            yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
-            stream = yt.streams.get_by_resolution(max_resolution) or yt.streams.filter().get_highest_resolution()
-    except Exception:
-        return 1
+    if only_audio:
+        stream = yt.streams.filter(only_audio=True).first()
+    else:
+        stream = yt.streams.get_by_resolution(max_resolution) or yt.streams.filter().get_highest_resolution()
 
     info = yt.vid_info
     author = info["videoDetails"]["author"]
@@ -42,13 +38,33 @@ def download_mp4(link, new_title, table=dict()):
     table["quality"] = stream.resolution
     table["size MB"] = stream.filesize_mb
 
-    try:
-        if not stream.exists_at_path(mp4_destination_path):
-            stream.download(MP4_DIRECTORY, filename=new_title + ".mp4")
-    except Exception:
-        return 3
-
+    if not stream.exists_at_path(mp4_destination_path):
+        stream.download(MP4_DIRECTORY, filename=new_title + ".mp4")
     maybe_download_thumbnail(yt, jpg_destination_path)
+
+
+def download_mp4(link, new_title, table=dict()):
+    link = link.replace("embed/", "watch?v=")
+
+    try:
+        return _download_mp4(link, new_title, table, authenticate=False, only_audio=False)
+    except Exception:
+        pass
+    if get_constant("INTERACTIVE_AUTHENTICATION"):
+        try:
+            return _download_mp4(link, new_title, table, authenticate=True, only_audio=False)
+        except Exception:
+            pass
+        try:
+            return _download_mp4(link, new_title, table, authenticate=True, only_audio=True)
+        except Exception as e:
+            print(e)
+    else:
+        try:
+            return _download_mp4(link, new_title, table, authenticate=False, only_audio=True)
+        except Exception as e:
+            print(e)
+    return 1  # non-zero return here means a failure
 
 
 def maybe_download_thumbnail(yt, destination):
